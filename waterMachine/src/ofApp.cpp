fb5e71f2deb12f464ce54471fe7f21680e6c9d53
Eric Jiang <erik.jjy@live.com>
2018-01-29 01:07:03 +0800

    add videograbber and sound plaer

--- waterMachine/src/ofApp.cpp
+++ waterMachine/src/ofApp.cpp
@@ -2,70 +2,50 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-
+    mCamWidth = 640;
+    mCamHeight = 480;
+    
+    mVideoGraber.setDeviceID(1);
+    mVideoGraber.setVerbose(true);
+    mVideoGraber.initGrabber(mCamWidth, mCamHeight);
+    mColorImage.allocate(mCamWidth, mCamHeight);
+    mGrayImage.allocate(mCamWidth, mCamHeight);
+    
+    mSounds.clear();
+    for (int i =0; i<=4; i++) {
+        ofSoundPlayer tempSound;
+        string fileName = "tone"+ofToString(i)+".mp3";
+        tempSound.load(fileName);
+        mSounds.push_back(tempSound);
+    }
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-
+    mVideoGraber.update();
+    if (mVideoGraber.isFrameNew()) {
+        mColorImage.setFromPixels(mVideoGraber.getPixels());
+    }
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-
+    ofSetBackgroundColor(0, 0, 0);
+//    mVideoGraber.draw(0, 0, mCamWidth,mCamHeight);
+    mColorImage.draw(0, 0);
 }
 
-//--------------------------------------------------------------
-void ofApp::keyPressed(int key){
-
-}
-
-//--------------------------------------------------------------
 void ofApp::keyReleased(int key){
 
+    switch (key){
+        case '0': mSounds[0].play();
+        case '1': mSounds[1].play();
+        case '2': mSounds[2].play();
+        case '3': mSounds[3].play();
+        case '4': mSounds[4].play();
+            
+    }
 }
 
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
 
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
-}
